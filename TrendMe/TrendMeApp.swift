21b0923ed3ce966202848fa1767ef34a83e584c7
Barbara Barone <babi.barone@cora.com.br>
2025-01-30 23:14:16 -0300

    Initial Commit

---
+++ TrendMe/TrendMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrendMeApp.swift
+//  TrendMe
+//
+//  Created by Barbara Barone on 30/01/25.
+//
+
+import SwiftUI
+
+@main
+struct TrendMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
