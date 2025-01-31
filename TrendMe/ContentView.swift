5f385d0d9ac875859d9c45a7d2f8cae4a2a1f328
Barbara Barone <babi.barone@cora.com.br>
2025-01-31 10:24:03 -0300

    adicionando configs iniciais

--- TrendMe/ContentView.swift
+++ TrendMe/ContentView.swift
@@ -1,24 +1,33 @@
-//
-//  ContentView.swift
-//  TrendMe
-//
-//  Created by Barbara Barone on 30/01/25.
-//
-
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            VStack(spacing: 24) {
+                Text("@TrendMe")
+                    .font(.system(size: 24, weight: .bold))
+                    .foregroundStyle(.white)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 24)
+                HStack(alignment: .center,spacing: 36) {
+                    Text("Casual")
+                    Text("Formal")
+                    Text("Street")
+                    Text("Luxury")
+                }
+                .font(.system(size: 14, weight: .regular))
+                .foregroundStyle(.white)
+                Color(.gray)
+                    .frame(width: 400, height: 350)
+                    .cornerRadius(20)
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    HomeView()
 }
--- TrendMe/TrendMeApp.swift
+++ TrendMe/TrendMeApp.swift
@@ -1,17 +1,10 @@
-//
-//  TrendMeApp.swift
-//  TrendMe
-//
-//  Created by Barbara Barone on 30/01/25.
-//
-
 import SwiftUI
 
 @main
 struct TrendMeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
