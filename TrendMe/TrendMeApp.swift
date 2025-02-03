886c4b80029b375eaf61c1649f08b8adea1a6a9f
Barbara Barone <babi.barone@cora.com.br>
2025-02-03 10:14:47 -0300

    configs finais do firebase

--- TrendMe/TrendMeApp.swift
+++ TrendMe/TrendMeApp.swift
@@ -1,7 +1,13 @@
+import FirebaseCore
 import SwiftUI
 
 @main
 struct TrendMeApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             HomeView()
